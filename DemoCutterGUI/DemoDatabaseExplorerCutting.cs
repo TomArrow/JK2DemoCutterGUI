895cccf707aac1bffffd5792ebf8157ebacf3640
Tom <tomarrowtomarrow@hotmail.com>
2023-12-11 17:12:13 +0100

    File name generation: - Add truncation offset to Rets - Add caps

--- DemoCutterGUI/DemoDatabaseExplorerCutting.cs
+++ DemoCutterGUI/DemoDatabaseExplorerCutting.cs
@@ -87,7 +87,7 @@ namespace DemoCutterGUI
             if(entry is Ret)
             {
                 Ret ret = entry as Ret;
-                if (entry == null) return null;
+                if (ret == null) return null;
                 string boostString = ((ret.boostCountAttacker + ret.boostCountVictim) > 0 ? ( "_BST" +  (ret.boostCountAttacker > 0 ? $"{ret.boostCountAttacker}A" : "") +( ret.boostCountVictim > 0 ? $"{ret.boostCountVictim}V" : "")) : "");
                 StringBuilder sb = new StringBuilder();
                 sb.Append(ret.map);
@@ -111,15 +111,83 @@ namespace DemoCutterGUI
                 sb.Append(ret.killerClientNum);
                 sb.Append("_");
                 sb.Append(ret.demoRecorderClientnum);
-                //(isTruncated ? va("_tr%d", truncationOffset) : "")
+
+                long demoTime = ret.demoTime.Value; // Just assume that demoTime exists. Otherwise there's nothing we can do anyway.
+                Int64 startTime = demoTime - preBuffertime;
+                Int64 endTime = demoTime + postBufferTime;
+                Int64 earliestPossibleStart = ret.lastGamestateDemoTime.GetValueOrDefault(0) + 1;
+                bool isTruncated = false;
+                Int64 truncationOffset = 0;
+                if (earliestPossibleStart > startTime)
+                {
+                    truncationOffset = earliestPossibleStart - startTime;
+                    startTime = earliestPossibleStart;
+                    isTruncated = true;
+                }
+
+                sb.Append(isTruncated ? $"_tr{truncationOffset}" : "");
                 sb.Append("_");
                 sb.Append(ret.shorthash);
 
 
                 retVal.demoName = sb.ToString();
-                long? demoTime = ret.demoTime;
-                retVal.demoTimeStart = !demoTime.HasValue ? -1: demoTime.Value - preBuffertime;
-                retVal.demoTimeEnd = !demoTime.HasValue ? -1: demoTime.Value + postBufferTime;
+                retVal.demoTimeStart = startTime;
+                retVal.demoTimeEnd = endTime;
+                return retVal;
+            } else if(entry is Capture)
+            {
+                Capture cap = entry as Capture;
+                if (cap == null) return null;
+                
+                StringBuilder sb = new StringBuilder();
+                sb.Append(cap.map);
+                sb.Append("___CAPTURE");
+                sb.Append(cap.capperKills > 0 ? $"{cap.capperKills}K" : "");
+                sb.Append(cap.capperRets > 0 ? $"{cap.capperRets}R": "");
+
+                sb.Append("___");
+                int milliSeconds = (int)cap.flagHoldTime.Value;
+                int pureMilliseconds = milliSeconds % 1000;
+                int seconds = milliSeconds / 1000;
+                int pureSeconds = seconds % 60;
+                int minutes = seconds / 60;
+
+                sb.Append(minutes.ToString("000"));
+                sb.Append("-");
+                sb.Append(pureSeconds.ToString("00"));
+                sb.Append("-");
+                sb.Append(pureMilliseconds.ToString("000"));
+
+                sb.Append($"___{cap.capperName}");
+                sb.Append($"___P{cap.flagPickupSource}T{cap.flagTeam}");
+                sb.Append($"___P{(int)cap.moreThanOneVeryCloseEnemyTimePercent}");
+                sb.Append($"DANGER{(int)(cap.averageVeryCloseEnemyCount*100)}");
+                sb.Append($"___{(int)cap.maxSpeedCapper}_{(int)cap.averageSpeedCapper}ups");
+                sb.Append(cap.capperWasFollowed == true ? "" : (cap.capperWasFollowedOrVisible == true ? "___thirdperson" : "___NOTvisible"));
+                sb.Append("_");
+                sb.Append(cap.capperClientNum);
+                sb.Append("_");
+                sb.Append(cap.demoRecorderClientnum);
+
+                long demoTime = cap.demoTime.Value; // Just assume that demoTime exists. Otherwise there's nothing we can do anyway.
+                Int64 startTime = demoTime - preBuffertime;
+                Int64 endTime = demoTime + postBufferTime;
+                Int64 earliestPossibleStart = cap.lastGamestateDemoTime.GetValueOrDefault(0) + 1;
+                bool isTruncated = false;
+                Int64 truncationOffset = 0;
+                if (earliestPossibleStart > startTime)
+                {
+                    truncationOffset = earliestPossibleStart - startTime;
+                    startTime = earliestPossibleStart;
+                    isTruncated = true;
+                }
+
+                sb.Append(isTruncated ? $"_tr{truncationOffset}" : "");
+
+
+                retVal.demoName = sb.ToString();
+                retVal.demoTimeStart = startTime;
+                retVal.demoTimeEnd = endTime;
                 return retVal;
             }
 
