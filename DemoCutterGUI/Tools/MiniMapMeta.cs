de3b5b7c26e0acb536e920a235f1acfbffb1ca65
Tom <tomarrowtomarrow@hotmail.com>
2024-04-30 03:31:11 +0200

    Draw map centered around selected kills/caps

--- DemoCutterGUI/Tools/MiniMapMeta.cs
+++ DemoCutterGUI/Tools/MiniMapMeta.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using OpenTK.Mathematics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +15,10 @@ namespace DemoCutterGUI.Tools
         public float maxX { get; set; }
         public float maxY { get; set; }
         public float maxZ { get; set; }
+
+        public Vector2 GetTexturePositionXY(Vector3 position)
+        {
+            return new Vector2() { X = (position.X - minX) / (maxX - minX), Y = (position.Y - minY) / (maxY - minY) };
+        }
     }
 }
--- DemoCutterGUI/Tools/MiniMapRenderer.cs
+++ DemoCutterGUI/Tools/MiniMapRenderer.cs
@@ -19,6 +19,28 @@ namespace DemoCutterGUI.Tools
     class MiniMapPoint {
         public Vector3 position;
         public bool main;
+
+        public class Bounds {
+            public Vector3 mins;
+            public Vector3 maxs;
+            public Vector3 GetCenter()
+            {
+                return (mins + maxs) / 2.0f;
+            }
+        }
+
+        public static Bounds GetBounds(MiniMapPoint[] points)
+        {
+            if (points is null || points.Length == 0) return null;
+            Vector3 maxs = new Vector3() { X=float.NegativeInfinity,Y=float.NegativeInfinity,Z=float.NegativeInfinity };
+            Vector3 mins = new Vector3() { X=float.PositiveInfinity,Y=float.PositiveInfinity, Z=float.PositiveInfinity };
+            foreach(MiniMapPoint point in points)
+            {
+                maxs = Vector3.ComponentMax(maxs, point.position);
+                mins = Vector3.ComponentMin(mins, point.position);
+            }
+            return new Bounds() { mins = mins, maxs=maxs }; 
+        }
     }
 
 
@@ -120,6 +142,8 @@ namespace DemoCutterGUI.Tools
 
                 GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.LinearMipmapLinear);
                 GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMinFilter.LinearMipmapLinear);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Clamp);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Clamp);
 
                 GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, img.Width, img.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, img.Data);
 
@@ -174,30 +198,56 @@ namespace DemoCutterGUI.Tools
 
             if (textureHandle < 0 || miniMapMeta is null) return; // No minimap texture found
 
+            MiniMapPoint[] points = items.ToArray();
+
+            MiniMapPoint.Bounds bounds = MiniMapPoint.GetBounds(points);
+            Vector3 center = bounds.GetCenter();
+
+            Vector3 boundsRanges = bounds.maxs - bounds.mins;
+            float xyRange = Math.Max(Math.Max(boundsRanges.X,boundsRanges.Y) + 100.0f, 2000.0f);
+            float xzRange = Math.Max(Math.Max(boundsRanges.X,boundsRanges.Z) + 100.0f, 2000.0f);
+            float yzRange = Math.Max(Math.Max(boundsRanges.Y,boundsRanges.Z) + 100.0f, 2000.0f);
+
+            Vector3 xyRangeHalfVec = Vector3.One * xyRange * 0.5f;
+            float xyRangeHalf = xyRange * 0.5f;
+
+            Vector2 topRightPos = miniMapMeta.GetTexturePositionXY(center + xyRangeHalfVec);
+            Vector2 bottomRightPos = miniMapMeta.GetTexturePositionXY(center + new Vector3() { X = xyRangeHalf, Y = -xyRangeHalf });
+            Vector2 bottomLeftPos = miniMapMeta.GetTexturePositionXY(center- xyRangeHalfVec);
+            Vector2 topLeftPos = miniMapMeta.GetTexturePositionXY(center + new Vector3() { X = -xyRangeHalf, Y = xyRangeHalf });
+
+
             GL.Enable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.Blend);
+
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
 
             GL.BindTexture(TextureTarget.Texture2D, textureHandle);
 
             GL.Begin(PrimitiveType.Quads);
 
-            GL.TexCoord2(1.0, 1.0);
+            //GL.TexCoord2(1.0, 1.0);
+            GL.TexCoord2(topRightPos);
             GL.Vertex2(1.0, 1.0);
 
-            GL.TexCoord2(1.0, 0.0);
+            //GL.TexCoord2(1.0, 0.0);
+            GL.TexCoord2(bottomRightPos);
             GL.Vertex2(1.0, -1.0);
 
-            GL.TexCoord2(0.0, 0.0);
+            //GL.TexCoord2(0.0, 0.0);
+            GL.TexCoord2(bottomLeftPos);
             GL.Vertex2(-1.0, -1.0);
 
-            GL.TexCoord2(0.0, 1.0);
+            //GL.TexCoord2(0.0, 1.0);
+            GL.TexCoord2(topLeftPos);
             GL.Vertex2(-1.0, 1.0);
             GL.End();
 
             GL.BindTexture(TextureTarget.Texture2D, 0);
 
             GL.Disable(EnableCap.Texture2D);
+            GL.Disable(EnableCap.Blend);
 
-            MiniMapPoint[] points = items.ToArray();
 
             lastUpdate = DateTime.Now;
 
